57fa80ec7d726d3ebad9bcd5ca47291a189acf7d
Joel Kosola <joel.kosola@eatech.fi>
2018-12-26 16:22:14 +0200

    Added OnMessage method

--- GettausBotti/Program.cs
+++ GettausBotti/Program.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
+using System.Threading;
 using Telegram.Bot;
+using Telegram.Bot.Args;
 
 namespace GettausBotti
 {
     class Program
     {
+        static ITelegramBotClient botClient;
+
         static void Main(string[] args)
         {
             //Set config file
@@ -14,12 +18,36 @@ namespace GettausBotti
             var configuration = builder.Build();
 
             //Init bot client
-            var botClient = new TelegramBotClient(configuration["accessToken"]);
+            botClient = new TelegramBotClient(configuration["accessToken"]);
             var me = botClient.GetMeAsync().Result;
 
             Console.WriteLine(
               $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
             );
+
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+            Thread.Sleep(int.MaxValue);
+        }
+
+
+        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            try
+            {
+                if (e.Message.Text != null)
+                {
+                    Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+                    await botClient.SendTextMessageAsync(
+                      chatId: e.Message.Chat,
+                      text: "You said:\n" + e.Message.Text
+                    );
+                }
+            }
+            catch(Exception ex){
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
